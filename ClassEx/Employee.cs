0936f00db5f575a43f8bfe161059f3ced6ddb157
ShravanMCAnu <bskmc850@gmail.com>
2022-11-01 09:56:09 +0530

    Add project files.

---
+++ ClassEx/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEx
+{
+    public class Employee
+    {
+        public string? EmpName { get; set; }
+        public string GetEmployee()
+        {
+            return EmpName;
+        }
+
+        public void SetEmployee(string _empName)
+        {
+            EmpName = _empName;
+        }
+    }
+}
---
+++ ClassEx/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassEx;
+
+Console.WriteLine("Hello, World!");
+
+Employee emp = new Employee();
+emp.SetEmployee("SRK");
+string sName=emp.GetEmployee();
+
+Console.WriteLine(sName);
